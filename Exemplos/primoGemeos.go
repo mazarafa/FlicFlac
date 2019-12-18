db11d46baebd1f176cbcb4142b8c3f8111f5286b
mazarafa7 <arruda.rafa.ra@gmail.com>
2019-12-18 19:01:42 -0300

    logica dos numeros primo gemeos

---
+++ Exemplos/primoGemeos.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int 
+	var y int 
+
+	fmt.Print("Informe o numero:")
+	fmt.Scanf("%d\n", &x)
+	fmt.Print("Informe o numero:")
+    fmt.Scanf("%d\n", &y)
+	var primo bool
+
+	for i := x; i < y+1; i++ {
+		primo = true
+
+		for j := 2; j < i; j++ {
+
+			if i%j == 0 {
+				primo = false
+				break
+			}
+		}
+
+		if primo {
+			fmt.Println(i)
+		}
+	}
+}
