4f0b37235921c44d42a3c87458adae27506de1c1
mazarafa7 <arruda.rafa.ra@gmail.com>
2019-12-27 20:38:02 -0300

    alterações feitas para ler os valores numa linha so

--- URI-Online/1047.go
+++ URI-Online/1047.go
@@ -15,54 +15,52 @@ func main() {
 	var x int 
 	var y int
 	
-	fmt.Scanf("%d\n", &a)
-	fmt.Scanf("%d\n", &x)
-	fmt.Scanf("%d\n", &b )
-	fmt.Scanf("%d\n", &y)
+	fmt.Scanf("%d %d %d %d\n", &a,&x,&b,&y)
+	
 	
 	if (a == y && b == y && y == x){
 		c=x-y
 		d=24+a-b
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c ,"MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c ,"MINUTO(S)")
 	}
 	if ( a == b && y > x) {
 		c=y-x
 		d=a-b
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)")
 	}
 	if ( a == b && x > y){
 		c=60-x+y
 		d=24-a+b-1
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)")
 	}
 	if (x == y && a < b){
 		c=0
 		d=b-a
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)")
 	}
 	if (x == y && a > b){
 		c=0
 		d=24-a+b
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)")
 	}
 	if (b > a && y > x){
 		c=y-x
 		d=b-a
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)")
 	}
 	if (a < b && x > y){
 		c=60-x+y
 		d=b-a-1
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)")
 	}
 	if (a > b && x < y){
 		c = y - x
 		d = 24 - a -1 + b
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)")
 	}
 	if (a > b && x > y){
 		c= 60 + y - x
 		d= 24 + b - a - 1
-		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)\n")
+		fmt.Println("O JOGO DUROU", d ,"HORA(S) E ", c , "MINUTO(S)")
 	}
 }
